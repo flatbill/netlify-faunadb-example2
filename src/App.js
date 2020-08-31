3367696b2e111a6dcf97b48efbd709d862b25e28
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-08-31 19:14:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -304,6 +304,7 @@ export default class App extends Component {
             </div>
           </label>
           {deleteButton}
+          <button> wingo </button>
         </div>
       )
     })
