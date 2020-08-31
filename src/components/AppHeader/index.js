2d6607f5c69f41b4718df3672f601d03c7fc188c
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-08-31 09:03:29 -0400

    Update index.js

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -14,7 +14,7 @@ const AppHeader = (props) => {
             <div className='app-title-text'>
               <h1 className='app-title'>Netlify + Fauna DB</h1>
               <p className='app-intro'>
-                Using FaunaDB & Netlify functions
+                Using FaunaDB & Netlify functionsss
               </p>
             </div>
           </div>
