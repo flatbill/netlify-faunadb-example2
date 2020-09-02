88830a7db16982c699e633a0e42d60fe135bac00
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-09-02 15:34:59 -0400

    Update index.js

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -3,17 +3,9 @@ import styles from './AppHeader.css' // eslint-disable-line
 
 const AppHeader = (props) => {
   return (
-    <header className='app-header'>
-      <div className='app-title-wrapper'>
-        <div className='app-title-wrapper'>
-          <div className='app-left-nav'>           
-            <div className='app-title-text'>
-              <h1 className='app-title'>Questool 2020 drEd5</h1>              
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+    <div className='app-title-text'>
+      <h1 className='app-title'>Questool 2020 drEd5</h1>              
+    </div>   
   )
 }
 
