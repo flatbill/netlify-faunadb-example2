6f93ee4811dbb1f2b610d4b87adf20a6921b3f6d
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-09-10 19:31:28 -0400

    Update readTodo2.js

--- functions/readTodo2.js
+++ functions/readTodo2.js
@@ -9,7 +9,7 @@ exports.handler = (event, context) => {
     secret: process.env.FAUNADB_SERVER_SECRET
   }) 
   const id = getId(event.path)
-  console.log(`Function 'readTodo2' invoked. Read id: ${id}`)
+  console.log(`Function 'readTodo2' invoked, mr space cadet. Read id: ${id}`)
   return client.query(q.Get(q.Ref(`classes/todos/${id}`)))
     .then((response) => {
       console.log('success', response)
